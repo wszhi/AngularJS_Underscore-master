66bcce76454c51d77d71976958c68cc3e350b093
wszhi <wszhi@users.noreply.github.com>
2015-12-24 19:15:12 +0800

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,14 +2,18 @@
   var myAPP = angular.module('myAPP', []);
   var currentnum=0;
   var count=9;
+  
   myAPP.controller('mainController', function($scope ) {
   	
   	currentnum = currentnum+count;
     $scope.books = data.books.slice(0, currentnum);
     $scope.selectedTip = "出版社";
-    $scope.backctrl=function(){
-    	document.getElementById("book-err").style.display="none";
+    $scope.searchvisible = false;
+    $scope.isShow = false;
+    $scope.showorhide = function () {
+        $scope.searchvisible = !$scope.searchvisible;
     }
+
     $scope.searchbookinlist = function(selectedTip,selectedGenre){
       var bookbuf=_.filter(data.books, function(onebook){ 
         if(selectedTip.name == "出版社"){
@@ -29,24 +33,27 @@
    	$scope.searchbook = function(searchContent){
    	  var bookbuf=_.filter(data.books, function(onebook){ 
         if(searchContent != null){
-          if(onebook.title.indexOf(searchContent) > 0)
+          if(_.contains(onebook.title, searchContent))
             return onebook;
         }
    	  });
-   	  if(bookbuf != "")
-   	  	$scope.books = bookbuf;
+   	  if(_.isEmpty(bookbuf)){
+        $scope.searchvisible = true;
+        $scope.isShow = true;
+      }
    	  else{
-   	  	document.getElementById("book-err-info").innerHTML="没有符合要求的书籍~~~~(>_<)~~~~";
-   	  	document.getElementById("book-err").style.display="block";
+        $scope.books = bookbuf;
+   	  	
    	  }
+      console.log($scope.searchvisible+"****"+$scope.isShow);
    }
    $scope.loadmorebook = function(){
    	var bookbuf2=data.books.slice(currentnum, currentnum+count);
    	 currentnum = currentnum+count;
    	 
-   	 if(bookbuf2 == ""){
-   	 	document.getElementById("book-err-info").innerHTML="没有更多的书籍了~~~~(>_<)~~~~";
-   	 	 document.getElementById("book-err").style.display="block";
+   	 if(_.isEmpty(bookbuf2)){
+        $scope.searchvisible = true;
+        $scope.isShow = false;
    	 }
    	 else
    	 	$scope.books = data.books.slice(0, currentnum);
@@ -62,15 +69,10 @@
  			selecttagbuf.push(_.pluck(onebook.tags, 'name'));
  			return _.pluck(onebook.tags, 'name');
  		});
- 		var tagbuf=[];
- 		for (var i = 0; i < selecttagbuf.length; i++){
- 			var arr=selecttagbuf[i];
- 			for(var j=0;j<arr.length;j++){
- 				tagbuf.push(arr[j]);
- 			}
- 		}
- 		var sellectpub=unique(selectpubbuf);
- 		var selecttag=unique(tagbuf);
+
+ 		var tagbuf=_.flatten(selecttagbuf);
+ 		var sellectpub=_.uniq(selectpubbuf);
+ 		var selecttag=_.uniq(tagbuf);
     $scope.selecttip = [
                     {
                         id: 0,
@@ -87,14 +89,3 @@
 
  
 })(angular, data, _);
-
-function unique(arr) {
-    var result = [], hash = {};
-    for (var i = 0, elem; (elem = arr[i]) != null; i++) {
-        if (!hash[elem]) {
-            result.push(elem);
-            hash[elem] = true;
-        }
-    }
-    return result;
-}
